5f773c1a23b1d4440f41c882fa0352005f610772
asifikbal09 <asif.ikbal662@gmail.com>
2023-08-14 01:47:32 +0600

    Menu Page Complite

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -2,22 +2,61 @@ import React from "react";
 import { Helmet } from "react-helmet-async";
 import Banner from "../../../component/Banner/Banner";
 import menuImg from "../../../assets/menu/banner3.jpg";
-import PopularMenu from "../../Home/PopularMenu/PopularMenu";
+import pizzaImg from "../../../assets/menu/pizza-bg.jpg";
+import saladImg from "../../../assets/menu/salad-bg.jpg";
+import soupImg from "../../../assets/menu/soup-bg.jpg";
+import dessertImg from "../../../assets/menu/dessert-bg.jpeg";
+import useMenu from "../../../Hooks/useMenu";
+import MenuCategory from "../MenuCategory/MenuCategory";
+import SectionTitle from "../../../component/SectionTitle/SectionTitle";
 
 const Menu = () => {
+  const [menu] = useMenu();
+  const dessert = menu.filter((item) => item.category === "dessert");
+  const pizza = menu.filter((item) => item.category === "pizza");
+  const salad = menu.filter((item) => item.category === "salad");
+  const offered = menu.filter((item) => item.category === "offered");
+  const soup = menu.filter((item) => item.category === "soup");
   return (
     <div>
       <Helmet>
         <title>Bistro Boss | Menu</title>
       </Helmet>
-      <Banner bgImg={menuImg} title='our menu' subTitle='would you like to try our dish?'></Banner>
-      <PopularMenu></PopularMenu>
-      <Banner bgImg={menuImg} title='our menu' subTitle='would you like to try our dish?'></Banner>
-      <PopularMenu></PopularMenu>
-      <Banner bgImg={menuImg} title='our menu' subTitle='would you like to try our dish?'></Banner>
-      <PopularMenu></PopularMenu>
-      <Banner bgImg={menuImg} title='our menu' subTitle='would you like to try our dish?'></Banner>
-      <PopularMenu></PopularMenu>
+      <Banner
+        bgImg={menuImg}
+        title="our menu"
+        subTitle="would you like to try our dish?"
+      ></Banner>
+      {/* today's offer */}
+      <SectionTitle
+        heading={"Today's Offers"}
+        subHeading={"Don't Miss"}
+      ></SectionTitle>
+      <MenuCategory items={offered}></MenuCategory>
+      {/* desserts */}
+      <MenuCategory
+        items={dessert}
+        title={"desserts"}
+        bannerImg={dessertImg}
+      ></MenuCategory>
+      {/* pizza */}
+      <MenuCategory
+        items={pizza}
+        title={"pizza"}
+        bannerImg={pizzaImg}
+      ></MenuCategory>
+      {/* salad */}
+      <MenuCategory
+        items={salad}
+        title={"salad"}
+        bannerImg={saladImg}
+      ></MenuCategory>
+      {/* soup */}
+      <MenuCategory
+        items={soup}
+        title={"soup"}
+        bannerImg={soupImg}
+      ></MenuCategory>
     </div>
   );
 };
--- src/Pages/Menu/MenuCategory/MenuCategory.jsx
+++ src/Pages/Menu/MenuCategory/MenuCategory.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import MenuItem from '../../../Shared/MenuItem/MenuItem';
+import Banner from '../../../component/Banner/Banner';
 
-const MenuCategory = () => {
+const MenuCategory = ({title,bannerImg,items}) => {
     return (
-        <div>
-            
+        <div className='py-6'>
+            {title && <Banner bgImg={bannerImg} title={title} subTitle='would you like to try our dish?'></Banner>}
+            <div className="grid md:grid-cols-2 gap-5 mt-16">
+        {items.map((menu) => (
+          <MenuItem key={menu._id} menu={menu}></MenuItem>
+        ))}
+      </div>
         </div>
     );
 };
