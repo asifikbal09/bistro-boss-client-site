8cd6e46cc38613f2e106f74e83b78dc245a037c7
asifikbal09 <asif.ikbal662@gmail.com>
2023-08-14 00:02:59 +0600

    Custom Hooks

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,27 +1,30 @@
 import { useEffect, useState } from "react";
 import SectionTitle from "../../../component/SectionTitle/SectionTitle";
 import MenuItem from "../../../Shared/MenuItem/MenuItem";
+import useMenu from "../../../Hooks/useMenu";
 const PopularMenu = () => {
-  const [menus, setMenus] = useState([]);
-  useEffect(() => {
-    fetch("menu.json")
-      .then((res) => res.json())
-      .then((data) => {
-        const popularItem = data.filter(item => item.category === "popular")
-        setMenus(popularItem)
-      });
-  }, []);
+  const [menu] = useMenu();
+  const popularItem = menu.filter((item) => item.category === "popular");
+  // const [menus, setMenus] = useState([]);
+  // useEffect(() => {
+  //   fetch("menu.json")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       const popularItem = data.filter(item => item.category === "popular")
+  //       setMenus(popularItem)
+  //     });
+  // }, []);
   return (
     <section className="mb-12">
       <SectionTitle
         heading="from our menu"
         subHeading="Check it out"
       ></SectionTitle>
-     <div className="grid md:grid-cols-2 gap-5">
-     {
-        menus.map(menu => <MenuItem key={menu._id} menu={menu}></MenuItem> )
-      }
-     </div>
+      <div className="grid md:grid-cols-2 gap-5">
+        {popularItem.map((menu) => (
+          <MenuItem key={menu._id} menu={menu}></MenuItem>
+        ))}
+      </div>
     </section>
   );
 };
--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { Helmet } from "react-helmet-async";
+import Banner from "../../../component/Banner/Banner";
+import menuImg from "../../../assets/menu/banner3.jpg";
+import PopularMenu from "../../Home/PopularMenu/PopularMenu";
 
 const Menu = () => {
   return (
@@ -7,7 +10,14 @@ const Menu = () => {
       <Helmet>
         <title>Bistro Boss | Menu</title>
       </Helmet>
-      <h1>meny menu menu</h1>
+      <Banner bgImg={menuImg} title='our menu' subTitle='would you like to try our dish?'></Banner>
+      <PopularMenu></PopularMenu>
+      <Banner bgImg={menuImg} title='our menu' subTitle='would you like to try our dish?'></Banner>
+      <PopularMenu></PopularMenu>
+      <Banner bgImg={menuImg} title='our menu' subTitle='would you like to try our dish?'></Banner>
+      <PopularMenu></PopularMenu>
+      <Banner bgImg={menuImg} title='our menu' subTitle='would you like to try our dish?'></Banner>
+      <PopularMenu></PopularMenu>
     </div>
   );
 };
